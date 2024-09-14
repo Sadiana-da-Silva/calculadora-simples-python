61ea1f78e25bb3125d309b500b02e0cf5890a6a1
Sadiana Manuel da Silva <138226024+Sadiana-da-Silva@users.noreply.github.com>
2024-09-14 11:01:02 -0300

    Update lista-compras.py

--- DesafioFinal/lista-compras.py
+++ DesafioFinal/lista-compras.py
@@ -1,25 +1,3 @@
-"""""
-Objetivo do desafio: Desenvolver um aplicativo que gerencie uma lista de compras que permita adicionar, remover e listar os produtos adicionados nela. 
-
-Para isso, o seu aplicativo precisa ter as seguintes funcionalidades: 
-
-Menu de Opções: O sistema deve fornecer um menu de opções para o usuário interagir. As opções devem ser as seguintes:  
-1. Adicionar produto 
-2. Remover produto  
-3. Pesquisar produtos   
-s. Sair do programa  
-  
-Adicionar Produto: O usuário deve poder adicionar um novo produto à lista de compras. O sistema deve solicitar informações sobre o nome, unidade de medida, quantidade e descrição do produto.   
-As opções de unidade devem ser:  
-- Quilograma  
-- Grama  
-- Litro  
-- Mililitro  
-- Unidade  
-- Metro  
-- Centímetro  
-Essas opções devem aparecer quando o sistema perguntar a unidade de medida.
-"""""
 import unicodedata
 
 
